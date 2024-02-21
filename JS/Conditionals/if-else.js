030795ee4a2ba1532c4f1def4c05a2345f37429e
NaveenReddy0427 <naveendeva909@gmail.com>
2024-02-21 13:00:45 +0530

    practice code for esle if

--- JS/Conditionals/if-else.js
+++ JS/Conditionals/if-else.js
@@ -80,3 +80,33 @@ switch(starInt1){
         console.log("i dont know what star rating you are giving for");
 }
 
+
+
+/* 
+Problem statement
+A grading system needs to be implemented for a school based on the marks obtained by the students on a test. Write a JS program to determine the grades of the students using conditional statements.
+
+The grades will be assigned in the result variable based on the following criteria:
+
+1- If the value of the marks variable is greater than or equal to 90, the value of the result variable should be "A grade"
+2- If the marks are between 80 and 89, the value of the result variable should be "B grade".
+3- If the marks are between 70 and 79, the value of the result variable should be "C grade".
+4- If the marks are between 60 and 69, the value of the result variable should be "D grade".
+5- If the marks are less than 60, the value of the result variable should be "F grade". 
+*/
+
+let marks = 100
+
+if(marks>=90){
+    console.log("Grade A");
+}else if(marks>=80 && 89){
+    console.log("Grade A");
+}else if(marks>=70 && 79){
+    console.log("Grade A");
+}else if(marks>=60 && 69){
+    console.log("Grade A");
+}else {(marks<60)
+    console.log("Grade A");
+}
+
+  
