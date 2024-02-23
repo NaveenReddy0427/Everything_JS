9f397300c41feb0d4151a9e3fc21d477f9213a6d
NaveenReddy0427 <naveendeva909@gmail.com>
2024-02-23 14:15:46 +0530

    seperating switch case code to the switch case file

--- JS/Conditionals/if-else.js
+++ JS/Conditionals/if-else.js
@@ -35,28 +35,7 @@ console.log(`your score is: ${score} and your life is: ${life} and your bullets 
 
 
 
-// switch case 
-let star1 = "5"
-let starInt1 = parseInt(star1)
-switch(starInt1){
-    case 1:
-        console.log("not okay");
-        break;
-    case 2:
-        console.log("emm ok");
-        break;
-    case 3:
-        console.log("good");
-        break;
-    case 4:
-        console.log("great");
-        break;
-    case 5:
-        console.log("awesome");
-        break;
-    default:
-        console.log("i dont know what star rating you are giving for");
-}
+
 
 
 
---
+++ JS/Conditionals/switchcase.js
@@ -0,0 +1,23 @@
+
+// switch case 
+let star1 = "5"
+let starInt1 = parseInt(star1)
+switch(starInt1){
+    case 1:
+        console.log("not okay");
+        break;
+    case 2:
+        console.log("emm ok");
+        break;
+    case 3:
+        console.log("good");
+        break;
+    case 4:
+        console.log("great");
+        break;
+    case 5:
+        console.log("awesome");
+        break;
+    default:
+        console.log("i dont know what star rating you are giving for");
+}
